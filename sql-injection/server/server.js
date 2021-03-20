301e22a99fa5f30f522f1efc8f136b26a4f4d4d4
nlemast <nlemaster47@gmail.com>
2021-03-20 16:37:10 -0400

    Add postgres as dependency and create route for creating users

--- sql-injection/server/server.js
+++ sql-injection/server/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
 
+const { createUser } = require('./controllers/dbController');
+
 const PORT = 3000;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .type('html')
+    .sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.post('/user', createUser, (req, res) => {
+  res.redirect('/');
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
